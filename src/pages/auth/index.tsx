1d85dc09a12b6d4410f712cf0252cecae6751699
biellil <biel1313biel@gmail.com>
2024-02-28 16:11:06 -0300

    feat: :sparkles: criação do formulario e hero

--- src/pages/auth/index.tsx
+++ src/pages/auth/index.tsx
@@ -1,15 +1,38 @@
 import hero from '../../assets/heroBarbershp.png'
-
-import { MainHero } from './styles'
+import { MainHero, SectionHero, Sectionform } from './styles'
 
 export function Auth() {
   return (
     <MainHero>
-      <section>
-        <h1>Meu aplicativo React com Vite</h1>
+      <SectionHero>
         <img src={hero} alt="Hero" />
-      </section>
-      <section></section>
+      </SectionHero>
+      <Sectionform>
+        <form>
+          <label htmlFor="email">Email</label>
+          <input
+            type="email"
+            id="email"
+            name="email"
+            placeholder="exemplo@gmail.com"
+          />
+
+          <label htmlFor="password">Senha</label>
+          <input type="password" id="password" name="password" />
+
+          <button type="submit">Entrar</button>
+
+          <div>
+            <button type="button">Facebook</button>
+            <button type="button">Google</button>
+          </div>
+
+          <a href="#">Esqueceu a senha?</a>
+          <a href="#" className="sign-up">
+            Não possui conta? Cadastre-se
+          </a>
+        </form>
+      </Sectionform>
     </MainHero>
   )
 }
--- src/pages/auth/styles.ts
+++ src/pages/auth/styles.ts
@@ -2,4 +2,37 @@ import styled from 'styled-components'
 
 export const MainHero = styled.main`
   display: flex;
+  height: 100vh;
+  background-color: #8085a5;
+  justify-content: space-between;
+  align-items: center;
+`
+
+export const SectionHero = styled.section`
+  display: flex;
+  flex: 1;
+  width: 10vh;
+
+  img {
+    padding: 1rem;
+    width: 40rem;
+    height: 20rem;
+  }
+`
+export const Sectionform = styled.section`
+  height: 100vh;
+  width: 90vh;
+  background-color: #eceff1;
+  display: flex;
+  flex-direction: column;
+  align-items: center;
+  justify-content: center;
+
+  > form {
+    padding: 3rem;
+    display: flex;
+    flex-direction: column;
+    justify-content: start;
+    align-items: start;
+  }
 `
