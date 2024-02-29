b1461e1a2b7282fbf3f96741159ce15a3dd4d16c
biellil <biel1313biel@gmail.com>
2024-02-29 14:30:46 -0300

    refactor: :fire: desativando Context:AuthProvider

--- src/App.tsx
+++ src/App.tsx
@@ -1,15 +1,12 @@
 import { BrowserRouter } from 'react-router-dom'
 import { Routers } from './Router'
-import { AuthProvider } from './components/Context'
 
 export function App() {
   return (
     <>
-      <AuthProvider>
-        <BrowserRouter>
-          <Routers />
-        </BrowserRouter>
-      </AuthProvider>
+      <BrowserRouter>
+        <Routers />
+      </BrowserRouter>
     </>
   )
 }
