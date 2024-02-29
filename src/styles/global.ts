a4f6b645faf0f9f379213a548efed0c8843bd59a
biellil <biel1313biel@gmail.com>
2024-02-29 15:49:43 -0300

    feat: :art:

--- src/styles/global.ts
+++ src/styles/global.ts
@@ -2,23 +2,74 @@ import { createGlobalStyle } from 'styled-components'
 
 export const GlobalStyle = createGlobalStyle`
 
-*{
+@import url('https://fonts.googleapis.com/css2?family=Averia+Sans+Libre:wght@400;700&display=swap');
+
+  * {
     margin: 0;
     padding: 0;
     box-sizing: border-box;
     outline: none;
     border: none;
-    
- 
-  
+    font-family: 'Averia Sans Libre';
+    text-decoration: none;
+
+
+    @font-face {
+    font-family: "Averia Sans Libre";
+    font-weight: 300;
+    font-style: normal;
   }
 
-  html{
+ 
+
+  @font-face {
+    font-family: "Averia Sans Libre";
+    font-weight: 400;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Averia Sans Libre";
+    font-weight: 400;
+    font-style: italic;
+  }
+
+  @font-face {
+    font-family: "Averia Sans Libre";
+    font-weight: 700;
+    font-style: normal;
+  }
+
+  @font-face {
+    font-family: "Averia Sans Libre";
+    font-weight: 700;
+    font-style: italic;
+  }
+
+
+
+  }
+  
+  html {
     font-size: 100%;
-}
-@media (max-width: 600px) {
+  }
+
+  
+  @media (max-width: 768px) {
     html {
-        font-size: 50%;
+      font-size: 80%;
     }
-}
+  }
+
+  @media (max-width: 600px) {
+    html {
+      font-size: 50%;
+    }
+  }
+
+  @media (max-width: 375px) {
+    html {
+      font-size: 40%;
+    }
+  }
 `
