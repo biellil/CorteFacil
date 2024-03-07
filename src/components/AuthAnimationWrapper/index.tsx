ccb51893d683cbe7b75c42c3342edd75f02465d1
biellil <biel1313biel@gmail.com>
2024-03-07 14:07:26 -0300

    style: :sparkles: adicionar efeito de escala

--- src/components/AuthAnimationWrapper/index.tsx
+++ src/components/AuthAnimationWrapper/index.tsx
@@ -1,14 +1,18 @@
-import React from 'react'
-import { animated, useSpring } from 'react-spring'
+import React from 'react';
+import { animated, useSpring } from 'react-spring';
 
 type AuthAnimationWrapperProps = {
-  children: React.ReactNode
-}
+  children: React.ReactNode;
+};
 
 export const AuthAnimationWrapper: React.FC<AuthAnimationWrapperProps> = ({
   children,
 }) => {
-  const props = useSpring({ opacity: 1, from: { opacity: 0 } })
+  // Updated to include `transform` for the `scale` property
+  const props = useSpring({
+    to: { opacity: 1, transform: 'scale(1)' },
+    from: { opacity: 0, transform: 'scale(0)' },
+  });
 
-  return <animated.div style={props}>{children}</animated.div>
-}
+  return <animated.div style={props}>{children}</animated.div>;
+};
