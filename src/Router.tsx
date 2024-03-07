24888f45ac2de8fed050a2a42af6903569445282
biellil <biel1313biel@gmail.com>
2024-03-07 13:34:51 -0300

    feat: :truck: Atualiza sistema de rotas com páginas de login e cadastro

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,12 +1,13 @@
-import { Route, Routes } from 'react-router-dom'
-import { Auth } from './pages/auth'
+import { Navigate, Route, Routes } from 'react-router-dom'
+import { LoginPage } from './components/auth/LoginPage'
+import { SignupPage } from './components/auth/SignupPage'
 
 export function Routers() {
   return (
     <Routes>
-      <Route path="/">
-        <Route index element={<Auth />} />
-      </Route>
+      <Route path="/" index element={<LoginPage />} />
+      <Route path="/signup" element={<SignupPage />} />
+      <Route path="/" element={<Navigate replace to="/" />} />
     </Routes>
   )
 }
