dfc6d3090b51018151d8f4ff3cb38e7bb8f108c8
biellil <biel1313biel@gmail.com>
2024-03-09 20:12:28 -0300

    refactor: :sparkles: mudando o Global Style para componente App e configurando e criando tema dark

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,24 @@
+import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
+import { ThemeProvider } from 'styled-components'
 import { Routers } from './Router'
+import { GlobalStyle } from './styles/global'
+import { darkTheme, lightTheme } from './styles/themes'
 
 export function App() {
+  const [theme, setTheme] = useState('dark')
+
+  const toggleTheme = () => {
+    theme === 'light' ? setTheme('dark') : setTheme('light')
+  }
   return (
     <>
-      <BrowserRouter>
-        <Routers />
-      </BrowserRouter>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        <GlobalStyle />
+        <BrowserRouter>
+          <Routers />
+        </BrowserRouter>
+      </ThemeProvider>
     </>
   )
 }
