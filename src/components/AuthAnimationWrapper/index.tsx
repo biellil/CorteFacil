56e0a61eb6f98d63f63d11687e67aa869fa65fef
biellil <biel1313biel@gmail.com>
2024-03-09 18:13:38 -0300

    refactor: :truck:

--- src/components/AuthAnimationWrapper/index.tsx
+++ src/components/AuthAnimationWrapper/index.tsx
@@ -1,18 +1,17 @@
-import React from 'react';
-import { animated, useSpring } from 'react-spring';
+import React from 'react'
+import { animated, useSpring } from 'react-spring'
 
 type AuthAnimationWrapperProps = {
-  children: React.ReactNode;
-};
+  children: React.ReactNode
+}
 
 export const AuthAnimationWrapper: React.FC<AuthAnimationWrapperProps> = ({
   children,
 }) => {
-  // Updated to include `transform` for the `scale` property
   const props = useSpring({
     to: { opacity: 1, transform: 'scale(1)' },
     from: { opacity: 0, transform: 'scale(0)' },
-  });
+  })
 
-  return <animated.div style={props}>{children}</animated.div>;
-};
+  return <animated.div style={props}>{children}</animated.div>
+}
