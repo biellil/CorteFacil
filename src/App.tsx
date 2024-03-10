ffe8d93055753c4601d2d6f715d6ef405f51cca3
biellil <biel1313biel@gmail.com>
2024-03-10 17:11:22 -0300

    refactor: :fire: removendo o useEffect de troca de tema

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useEffect, useState } from 'react'
+import { useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Routers } from './Router'
@@ -6,15 +6,7 @@ import { GlobalStyle } from './styles/global'
 import { darkTheme, lightTheme } from './styles/themes'
 
 export function App() {
-  const [theme, setTheme] = useState('dark')
-
-  useEffect(() => {
-    const preferredTheme = window.matchMedia('(prefers-color-scheme: dark)')
-      .matches
-      ? 'dark'
-      : 'light'
-    setTheme(preferredTheme)
-  }, [])
+  const [theme /*, setTheme */] = useState('dark')
 
   /* const toggleTheme = () => {
     theme === 'light' ? setTheme('dark') : setTheme('light')
