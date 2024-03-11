b28a93aa9dc0d9890c23dc03a7c6fb7bed15bf8b
biellil <biel1313biel@gmail.com>
2024-03-11 19:21:28 -0300

    feat: :sparkles: criando context de função de troca de temas para acessar função de forma global

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,40 @@
-import { useState } from 'react'
+import { createContext, useContext, useState } from 'react'
 import { BrowserRouter } from 'react-router-dom'
 import { ThemeProvider } from 'styled-components'
 import { Routers } from './Router'
 import { GlobalStyle } from './styles/global'
 import { darkTheme, lightTheme } from './styles/themes'
 
-export function App() {
-  const [theme /*, setTheme */] = useState('dark')
 
-  /* const toggleTheme = () => {
-    theme === 'light' ? setTheme('dark') : setTheme('light')
-  } */
+const ThemeContext = createContext()
+
+export const useTheme = () => useContext(ThemeContext)
+
+export const CustomThemeProvider = ({ children }) => {
+  const [theme, setTheme] = useState('light')
+
+  const toggleTheme = () => {
+    setTheme(theme === 'light' ? 'dark' : 'light')
+  }
+
+  return (
+    <ThemeContext.Provider value={{ theme, toggleTheme }}>
+      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+        {children}
+      </ThemeProvider>
+    </ThemeContext.Provider>
+  )
+}
+
+export function App() {
   return (
     <>
-      <ThemeProvider theme={theme === 'light' ? lightTheme : darkTheme}>
+      <CustomThemeProvider>
         <GlobalStyle />
         <BrowserRouter>
           <Routers />
         </BrowserRouter>
-      </ThemeProvider>
+      </CustomThemeProvider>
     </>
   )
 }
