e492a95e35d3cb47d0f315aef102b2fa1c297569
biellil <biel1313biel@gmail.com>
2024-03-24 23:45:14 -0300

    test: :sparkles: ajustes de rotas e adicionando rota test

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,15 +1,17 @@
 import React, { Suspense } from 'react'
-import { Route, Routes } from 'react-router-dom'
+import { Navigate, Route, Routes } from 'react-router-dom'
 import { Loading } from './components/Loading'
 import { Serviços } from './pages/servico'
+import { Test } from './pages/test'
 
 const LazyAuth = React.lazy(() => import('./pages/auth'))
 
 export function Routers() {
   return (
     <Routes>
+      <Route path="/" element={<Navigate to="/Agendar" />} />
       <Route
-        path="/"
+        path="/Agendar"
         element={
           <Suspense fallback={<Loading />}>
             <LazyAuth />
@@ -17,6 +19,7 @@ export function Routers() {
         }
       />
       <Route path="/serviço" element={<Serviços />} />
+      <Route path="/Test" element={<Test />} />
     </Routes>
   )
 }
