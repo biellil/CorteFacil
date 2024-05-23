12e38a63f2359d97bc339dd61652bfdd45ddb1b5
biel.lil <biel1313biel@gmail.com>
2024-05-23 16:39:17 -0300

    feat: :truck: Simplificar roteamento no arquivo de rotas
    Esta alteração simplifica o arquivo de rotas `Routers.tsx` removendo a importação e uso desnecessário de Suspense e React.lazy para os componentes Auth e Serviços, agora importados diretamente no arquivo. Isso melhora a legibilidade do código e simplifica a estrutura de roteamento.

--- src/Router.tsx
+++ src/Router.tsx
@@ -1,40 +1,24 @@
-import React, { Suspense } from 'react'
 import { Navigate, Route, Routes } from 'react-router-dom'
 import { DefaultLayout } from './components/DefaultLayout'
-import { Loading } from './components/Loading'
+
 import Feat from './pages/feat'
 
 import { Agendar } from './pages/agendar'
 import { Test } from './pages/test'
+import { Auth } from './pages/auth'
+import Serviços from './pages/serviços'
 
-const LazyAuth = React.lazy(() => import('./pages/auth'))
-const LazyServiços = React.lazy(() => import('./pages/serviços'))
 export function Routers() {
   return (
     <Routes>
       <Route path="/" element={<Navigate to="/Feat" />} />
-      <Route
-        path="/Auth"
-        element={
-          <Suspense fallback={<Loading />}>
-            <LazyAuth />
-          </Suspense>
-        }
-      />
+      <Route path="/Auth" element={<Auth />} />
 
       <Route path="/" element={<DefaultLayout />}>
-        <Route
-          path="/Serviço"
-          element={
-            <Suspense fallback={<Loading />}>
-              <LazyServiços />
-            </Suspense>
-          }
-        />
+        <Route path="/Serviço" element={<Serviços />} />
         <Route path="/Test" element={<Test />} />
         <Route path="/Feat" element={<Feat />} />
         <Route path="/Agendar" element={<Agendar />} />
-        {'Se for adicionar mais rotas, coloque-as aqui'}
       </Route>
     </Routes>
   )
